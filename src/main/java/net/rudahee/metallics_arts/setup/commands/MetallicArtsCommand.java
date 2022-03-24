d59130e23897b269ed693a79b2701d852bdcf8d5
TobibusFate <tovibur@gmail.com>
2022-03-24 11:27:45 -0300

    meh

---
+++ src/main/java/net/rudahee/metallics_arts/setup/commands/MetallicArtsCommand.java
@@ -0,0 +1,12 @@
+package net.rudahee.metallics_arts.setup.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.command.arguments.EntityArgument;
+
+public class MetallicArtsCommand {
+
+
+}
