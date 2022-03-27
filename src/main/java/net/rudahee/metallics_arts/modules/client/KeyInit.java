b6188f745767b297c01c0c406650b758fd5ec375
TobibusFate <tovibur@gmail.com>
2022-03-27 14:58:56 -0300

    ª

--- src/main/java/net/rudahee/metallics_arts/modules/client/KeyInit.java
+++ src/main/java/net/rudahee/metallics_arts/modules/client/KeyInit.java
@@ -11,15 +11,23 @@ import org.lwjgl.glfw.GLFW;
 @OnlyIn(Dist.CLIENT)
 public class KeyInit {
 
+
+
+    //particulas y sonidos
+
     @OnlyIn(Dist.CLIENT)
     public static KeyBinding allomancy;
 
     @OnlyIn(Dist.CLIENT)
     public static KeyBinding feruchemic;
 
+    @OnlyIn(Dist.CLIENT)
+    public static KeyBinding hud;
+
     public static void register (){
         feruchemic = new KeyBinding("key." + MetallicsArts.MOD_ID + "." + "Allomancy", GLFW.GLFW_KEY_N, "Metallic Arts");
         allomancy = new KeyBinding("key." + MetallicsArts.MOD_ID + "." + "Feruchemic", GLFW.GLFW_KEY_M, "Metallic Arts");
+        hud = new KeyBinding("key." + MetallicsArts.MOD_ID + "." + "Hud", GLFW.GLFW_KEY_O, "Metallic Arts");
 
         ClientRegistry.registerKeyBinding(feruchemic);
         ClientRegistry.registerKeyBinding(allomancy);
