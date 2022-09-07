edb7abbdb2fc6f132f057976cc383dbaff0a2150
Tobias Burger <tovibur@gmail.com>
2022-09-07 12:00:39 -0300

    algo

--- src/main/java/net/rudahee/metallics_arts/modules/data_player/InvestedDataProvider.java
+++ src/main/java/net/rudahee/metallics_arts/modules/data_player/InvestedDataProvider.java
@@ -3,6 +3,8 @@ package net.rudahee.metallics_arts.modules.data_player;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 
--- src/main/java/net/rudahee/metallics_arts/setup/registries/ModItems.java
+++ src/main/java/net/rudahee/metallics_arts/setup/registries/ModItems.java
@@ -86,6 +86,9 @@ public class ModItems {
         });
 
 
+
+
+
     }
 
     //MentalMinds
