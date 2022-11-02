0405ab491845b1ae1efb290ba8f07f7c1908d4cb
unknown <sregio03@gmail.com>
2022-11-02 19:31:48 +0100

    Book added to the correct creativeTab

---
+++ src/main/java/net/rudahee/metallics_arts/modules/items/banners/IronBanner.java
@@ -0,0 +1,15 @@
+package net.rudahee.metallics_arts.modules.items.banners;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BannerItem;
+import net.minecraft.world.item.BannerPatternItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BannerPattern;
+
+public class IronBanner extends BannerPatternItem {
+
+    public IronBanner(TagKey<BannerPattern> p_220008_, Properties properties) {
+        super(p_220008_, properties);
+    }
+
+}
