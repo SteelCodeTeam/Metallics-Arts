e65c937b00ce7ce952aaec6ad6c30634c964db21
unknown <sregio03@gmail.com>
2022-12-01 12:55:46 +0100

    Modified paintings and spanish translation class improved

--- src/main/java/net/rudahee/metallics_arts/data/providers/ModLanguageProviderES.java
+++ src/main/java/net/rudahee/metallics_arts/data/providers/ModLanguageProviderES.java
@@ -2,6 +2,7 @@ package net.rudahee.metallics_arts.data.providers;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
+import net.rudahee.metallics_arts.modules.powers.client.MetalBlockHelpers;
 import net.rudahee.metallics_arts.setup.enums.extras.MetalsNBTData;
 import net.rudahee.metallics_arts.setup.enums.langs.MetalSpanish;
 
@@ -12,53 +13,11 @@ public class ModLanguageProviderES extends LanguageProvider {
 
 
     private Map<String, String> base = new HashMap<>() {{
-        put("item.metallics_arts.steel_ingot", "Lingote de acero");
-        put("item.metallics_arts.tin_ingot", "Lingote de estaño");
-        put("item.metallics_arts.pewter_ingot", "Lingote de peltre");
-        put("item.metallics_arts.bronze_ingot", "Lingote de bronce");
-        put("item.metallics_arts.zinc_ingot", "Lingote de zinc");
-        put("item.metallics_arts.brass_ingot", "Lingote de latón");
-        put("item.metallics_arts.electrum_ingot", "Lingote de electrum");
-        put("item.metallics_arts.cadmium_ingot", "Lingote de cadmio");
-        put("item.metallics_arts.bendalloy_ingot", "Lingote de bendaleo");
-        put("item.metallics_arts.aluminum_ingot", "Lingote de aluminio");
-        put("item.metallics_arts.duralumin_ingot", "Lingote de duralumín");
-        put("item.metallics_arts.chromium_ingot", "Lingote de cromo");
-        put("item.metallics_arts.nicrosil_ingot", "Lingote de nicrosil");
-
-        put("item.metallics_arts.silver_ingot", "Lingote de plata");
-        put("item.metallics_arts.lead_ingot", "Lingote de plomo");
-        put("item.metallics_arts.nickel_ingot", "Lingote de níquel");
 
         put("item.metallics_arts.atium", "Atium");
         put("item.metallics_arts.malatium", "Malatium");
         put("item.metallics_arts.lerasium", "Lerasium");
         put("item.metallics_arts.ettmetal", "Ettmetal");
-
-
-        put("item.metallics_arts.steel_nugget", "Pepita de acero");
-        put("item.metallics_arts.tin_nugget", "Pepita de estaño");
-        put("item.metallics_arts.pewter_nugget", "Pepita de peltre");
-        put("item.metallics_arts.bronze_nugget", "Pepita de bronce");
-        put("item.metallics_arts.copper_nugget", "Pepita de cobre");
-        put("item.metallics_arts.zinc_nugget", "Pepita de zinc");
-        put("item.metallics_arts.brass_nugget", "Pepita de latón");
-        put("item.metallics_arts.electrum_nugget", "Pepita de electrum");
-        put("item.metallics_arts.cadmium_nugget", "Pepita de cadmio");
-        put("item.metallics_arts.bendalloy_nugget", "Pepita de bendaleo");
-        put("item.metallics_arts.aluminum_nugget", "Pepita de aluminio");
-        put("item.metallics_arts.duralumin_nugget", "Pepita de duralumín");
-        put("item.metallics_arts.chromium_nugget", "Pepita de cromo");
-        put("item.metallics_arts.nicrosil_nugget", "Pepita de nicrosil");
-
-        put("item.metallics_arts.silver_nugget", "Pepita de plata");
-        put("item.metallics_arts.lead_nugget", "Pepita de plomo");
-        put("item.metallics_arts.nickel_nugget", "Pepita de níquel");
-
-        put("item.metallics_arts.atium_nugget", "Pepita de atium");
-        put("item.metallics_arts.malatium_nugget", "Pepita de malatium");
-        put("item.metallics_arts.lerasium_nugget", "Pepita de lerasium");
-        put("item.metallics_arts.ettmetal_nugget", "Pepita de ettmetal");
 
 
         put("item.metallics_arts.raw_tin", "Estaño en bruto");
@@ -86,6 +45,7 @@ public class ModLanguageProviderES extends LanguageProvider {
         put("block.metallics_arts.deepslate_silver_ore", "Mena de plata de pizarra profunda");
         put("block.metallics_arts.deepslate_lead_ore", "Mena de plomo de pizarra profunda");
         put("block.metallics_arts.deepslate_nickel_ore", "Mena de níquel de pizarra profunda");
+        put("block.metallics_arts.deepslate_cadmium_ore", "Mena de cadmio de pizarra profunda");
 
         put("block.metallics_arts.raw_tin_block", "Bloque de estaño en bruto");
         put("block.metallics_arts.raw_zinc_block", "Bloque de zinc en bruto");
@@ -97,29 +57,6 @@ public class ModLanguageProviderES extends LanguageProvider {
         put("block.metallics_arts.raw_lead_block", "Bloque de plomo en bruto");
         put("block.metallics_arts.raw_nickel_block", "Bloque de níquel en bruto");
 
-
-        put("block.metallics_arts.steel_block", "Bloque de acero");
-        put("block.metallics_arts.tin_block", "Bloque de estaño");
-        put("block.metallics_arts.pewter_block", "Bloque de peltre");
-        put("block.metallics_arts.bronze_block", "Bloque de bronce");
-        put("block.metallics_arts.zinc_block", "Bloque de zinc");
-        put("block.metallics_arts.brass_block", "Bloque de latón");
-        put("block.metallics_arts.electrum_block", "Bloque de electrum");
-        put("block.metallics_arts.cadmium_block", "Bloque de cadmio");
-        put("block.metallics_arts.bendalloy_block", "Bloque de bendaleo");
-        put("block.metallics_arts.aluminum_block", "Bloque de aluminio");
-        put("block.metallics_arts.duralumin_block", "Bloque de duralumín");
-        put("block.metallics_arts.chromium_block", "Bloque de cromo");
-        put("block.metallics_arts.nicrosil_block", "Bloque de nicrosil");
-
-        put("block.metallics_arts.silver_block", "Bloque de plata");
-        put("block.metallics_arts.lead_block", "Bloque de plomo");
-        put("block.metallics_arts.nickel_block", "Bloque de níquel");
-
-        put("block.metallics_arts.atium_block", "Bloque de atium");
-        put("block.metallics_arts.malatium_block", "Bloque de malatium");
-        put("block.metallics_arts.lerasium_block", "Bloque de lerasium");
-        put("block.metallics_arts.ettmetal_block", "Bloque de ettmetal");
 
         put("item.metallics_arts.band_aluminum_duralumin", "Brazal de aluminio-duralumín");
         put("item.metallics_arts.band_atium_malatium", "Brazal de atium-malatium");
@@ -143,26 +80,6 @@ public class ModLanguageProviderES extends LanguageProvider {
         put("item.metallics_arts.ring_zinc_brass", "Anillo de zinc-latón");
         put("item.metallics_arts.ring_steel_iron", "Anillo de hierro-acero");
 
-        put("item.metallics_arts.iron_spike", "Clavo de hierro");
-        put("item.metallics_arts.steel_spike", "Clavo de acero");
-        put("item.metallics_arts.tin_spike", "Clavo de estaño");
-        put("item.metallics_arts.pewter_spike", "Clavo de peltre");
-        put("item.metallics_arts.copper_spike", "Clavo de cobre");
-        put("item.metallics_arts.bronze_spike", "Clavo de bronce");
-        put("item.metallics_arts.zinc_spike", "Clavo de zinc");
-        put("item.metallics_arts.brass_spike", "Clavo de latón");
-        put("item.metallics_arts.chromium_spike", "Clavo de cromo");
-        put("item.metallics_arts.nicrosil_spike", "Clavo de nicrosil");
-        put("item.metallics_arts.aluminum_spike", "Clavo de aluminio");
-        put("item.metallics_arts.duralumin_spike", "Clavo de duralumín");
-        put("item.metallics_arts.cadmium_spike", "Clavo de cadmio");
-        put("item.metallics_arts.bendalloy_spike", "Clavo de bendaleo");
-        put("item.metallics_arts.electrum_spike", "Clavo de electrum");
-        put("item.metallics_arts.gold_spike", "Clavo de oro");
-        put("item.metallics_arts.atium_spike", "Clavo de atium");
-        put("item.metallics_arts.malatium_spike", "Clavo de malatium");
-        put("item.metallics_arts.lerasium_spike", "Clavo de lerasium");
-        put("item.metallics_arts.ettmetal_spike", "Clavo de ettmetal");
 
         put("item.metallics_arts.obsidian_dagger", "Daga de obsidiana");
         put("item.metallics_arts.cristal_dagger", "Daga de cristal");
@@ -211,53 +128,6 @@ public class ModLanguageProviderES extends LanguageProvider {
         put("key.metallics_arts.feruchemic", "Selector de metal feruquímico");
         put("key.metallics_arts.vertical_jump", "Empuje vertical");
 
-        put("item.metallics_arts.aluminum_allomantic_icon", "Aluminio Alomantico");
-        put("item.metallics_arts.duralumin_allomantic_icon", "Duraluminio Alomantico");
-        put("item.metallics_arts.steel_allomantic_icon", "Acero Alomantico");
-        put("item.metallics_arts.iron_allomantic_icon", "Hierro Alomantico");
-        put("item.metallics_arts.tin_allomantic_icon", "Estaño Alomantico");
-
-        put("item.metallics_arts.pewter_allomantic_icon", "Peltre Alomantico");
-        put("item.metallics_arts.bronze_allomantic_icon", "Bronce Alomantico");
-        put("item.metallics_arts.copper_allomantic_icon", "Cobre Alomantico");
-        put("item.metallics_arts.zinc_allomantic_icon", "Zinc Alomantico");
-        put("item.metallics_arts.brass_allomantic_icon", "Latón Alomantico");
-
-        put("item.metallics_arts.gold_allomantic_icon", "Oro Alomantico");
-        put("item.metallics_arts.electrum_allomantic_icon", "Electrum Alomantico");
-        put("item.metallics_arts.cadmium_allomantic_icon", "Cadmio Alomantico");
-        put("item.metallics_arts.bendalloy_allomantic_icon", "Bendaleo Alomantico");
-        put("item.metallics_arts.chromium_allomantic_icon", "Cromo Alomantico");
-
-        put("item.metallics_arts.nicrosil_allomantic_icon", "Nicrosil Alomantico");
-        put("item.metallics_arts.atium_allomantic_icon", "Atium Alomantico");
-        put("item.metallics_arts.malatium_allomantic_icon", "Malatium Alomantico");
-        put("item.metallics_arts.lerasium_allomantic_icon", "Lerasium Alomantico");
-        put("item.metallics_arts.ettmetal_allomantic_icon", "Ettmetal Alomantico");
-
-        put("item.metallics_arts.aluminum_feruchemic_icon", "Aluminio Feruquimico");
-        put("item.metallics_arts.duralumin_feruchemic_icon", "Duraluminio Feruquimico");
-        put("item.metallics_arts.steel_feruchemic_icon", "Acero Feruquimico");
-        put("item.metallics_arts.iron_feruchemic_icon", "Hierro Feruquimico");
-        put("item.metallics_arts.tin_feruchemic_icon", "Estaño Feruquimico");
-
-        put("item.metallics_arts.pewter_feruchemic_icon", "Peltre Feruquimico");
-        put("item.metallics_arts.bronze_feruchemic_icon", "Bronce Feruquimico");
-        put("item.metallics_arts.copper_feruchemic_icon", "Cobre Feruquimico");
-        put("item.metallics_arts.zinc_feruchemic_icon", "Zinc Feruquimico");
-        put("item.metallics_arts.brass_feruchemic_icon", "Latón Feruquimico");
-
-        put("item.metallics_arts.gold_feruchemic_icon", "Oro Feruquimico");
-        put("item.metallics_arts.electrum_feruchemic_icon", "Electrum Feruquimico");
-        put("item.metallics_arts.cadmium_feruchemic_icon", "Cadmio Feruquimico");
-        put("item.metallics_arts.bendalloy_feruchemic_icon", "Bendaleo Feruquimico");
-        put("item.metallics_arts.chromium_feruchemic_icon", "Cromo Feruquimico");
-
-        put("item.metallics_arts.nicrosil_feruchemic_icon", "Nicrosil Feruquimico");
-        put("item.metallics_arts.atium_feruchemic_icon", "Atium Feruquimico");
-        put("item.metallics_arts.malatium_feruchemic_icon", "Malatium Feruquimico");
-        put("item.metallics_arts.lerasium_feruchemic_icon", "Lerasium Feruquimico");
-        put("item.metallics_arts.ettmetal_feruchemic_icon", "Ettmetal Feruquimico");
 
         put("metallics_arts.patchouli.name_book", "Artes Metalicas, Guia");
 
@@ -267,26 +137,6 @@ public class ModLanguageProviderES extends LanguageProvider {
 
         put("metallics_arts.mental_mind.nobody", "Sin Propietario");
 
-        put("metallics_arts.metal_translate.iron", "Hierro");
-        put("metallics_arts.metal_translate.steel", "Acero");
-        put("metallics_arts.metal_translate.tin", "Estaño");
-        put("metallics_arts.metal_translate.pewter", "Peltre");
-        put("metallics_arts.metal_translate.zinc", "Zinc");
-        put("metallics_arts.metal_translate.brass", "Latón");
-        put("metallics_arts.metal_translate.copper", "Cobre");
-        put("metallics_arts.metal_translate.bronze", "Bronce");
-        put("metallics_arts.metal_translate.aluminum", "Aluminio");
-        put("metallics_arts.metal_translate.duralumin", "Duraluminio");
-        put("metallics_arts.metal_translate.chromium", "Cromo");
-        put("metallics_arts.metal_translate.nicrosil", "Nicrosil");
-        put("metallics_arts.metal_translate.gold", "Oro");
-        put("metallics_arts.metal_translate.electrum", "Electrum");
-        put("metallics_arts.metal_translate.bendalloy", "Bendaleo");
-        put("metallics_arts.metal_translate.cadmium", "Cadmio");
-        put("metallics_arts.metal_translate.atium", "Atium");
-        put("metallics_arts.metal_translate.malatium", "Malatium");
-        put("metallics_arts.metal_translate.lerasium", "Lerasium");
-        put("metallics_arts.metal_translate.ettmetal", "Ettmetal");
 
         put("metallics_arts.mental_mind_translate.has_reserve", "Tiene Reservas Alomanticas");
         put("metallics_arts.mental_mind_translate.not_has_reserve", "No Tiene Reservas Alomanticas");
@@ -308,28 +158,67 @@ public class ModLanguageProviderES extends LanguageProvider {
         put("key.metallics_arts.feruchemic_store", "Almacenaje Feruqimico");
         put("key.metallics_arts.switch_overlay", "Alternar interfaz");
 
-        put("key.metallics_arts.iron_power", "Hierro");
-        put("key.metallics_arts.steel_power", "Acero");
-        put("key.metallics_arts.tin_power", "Estaño");
-        put("key.metallics_arts.pewter_power", "Peltre");
-        put("key.metallics_arts.copper_power", "Cobre");
-        put("key.metallics_arts.bronze_power", "Bronce");
-        put("key.metallics_arts.zinc_power", "Zinc");
-        put("key.metallics_arts.brass_power", "Laton");
-        put("key.metallics_arts.chromium_power", "Cromo");
-        put("key.metallics_arts.nicrosil_power", "Nicrosil");
-        put("key.metallics_arts.aluminum_power", "Aluminio");
-        put("key.metallics_arts.duralumin_power", "Duraluminio");
-        put("key.metallics_arts.cadmium_power", "Cadmio");
-        put("key.metallics_arts.bendalloy_power", "Bendaleo");
-        put("key.metallics_arts.electrum_power", "Electrum");
-        put("key.metallics_arts.gold_power", "Oro");
-        put("key.metallics_arts.atium_power", "Atium");
-        put("key.metallics_arts.malatium_power", "Malatium");
-        put("key.metallics_arts.lerasium_power", "Lerasium");
-        put("key.metallics_arts.ettmetal_power", "Ettmetal");
     }};
 
+
+    private Map<String, String> ingots = new HashMap<>() {{
+
+        int cont = 0;
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            if(cont <=16){
+                put("item.metallics_arts."+ metal.getNameLower() +"_ingot","Lingote de "+ MetalSpanish.valueOf(metal.name()).getMetalNameLower());
+                cont++;
+            }
+        }
+
+    }};
+
+    private Map<String, String> nuggets = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("item.metallics_arts."+ metal.getNameLower() +"_nugget","Pepita de "+ MetalSpanish.valueOf(metal.name()).getMetalNameLower());
+        }
+
+    }};
+    private Map<String, String> blocks = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("block.metallics_arts."+ metal.getNameLower() +"_block","Bloque de "+ MetalSpanish.valueOf(metal.name()).getMetalNameLower());
+        }
+
+    }};
+
+    private Map<String, String> spikes = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("item.metallics_arts."+ metal.getNameLower() +"_spìke","Clavo de "+ MetalSpanish.valueOf(metal.name()).getMetalNameLower());
+        }
+
+    }};
+    private Map<String, String> icons = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("item.metallics_arts."+ metal.getNameLower()+"_allomantic_icon" , MetalSpanish.valueOf(metal.name()).getMetalName()+" Alomantico");
+            put("item.metallics_arts."+ metal.getNameLower()+"_feruchemic_icon" , MetalSpanish.valueOf(metal.name()).getMetalName()+" Feruquimico");
+        }
+
+    }};
+
+    private Map<String, String> metalls = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("metallics_arts.metal_translate."+ metal.getNameLower() , MetalSpanish.valueOf(metal.name()).getMetalName());
+        }
+
+    }};
+
+    private Map<String, String> powers = new HashMap<>() {{
+
+        for (MetalsNBTData metal: MetalsNBTData.values()) {
+            put("key.metallics_arts."+ metal.getNameLower() +"_power", MetalSpanish.valueOf(metal.name()).getMetalName());
+        }
+
+    }};
     private Map<String, String> symbols = new HashMap<>() {{
         for (MetalsNBTData metal: MetalsNBTData.values()) {
             put("f_"+metal.getNameLower()+"_1", MetalSpanish.valueOf(metal.name()).getMetalName() + " feruquimico sombreado");
@@ -374,6 +263,34 @@ public class ModLanguageProviderES extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
+
+        for (String key: ingots.keySet()){
+            add(key, ingots.get(key));
+        }
+
+        for (String key: nuggets.keySet()){
+            add(key, nuggets.get(key));
+        }
+
+        for (String key: blocks.keySet()){
+            add(key, blocks.get(key));
+        }
+
+        for (String key: spikes.keySet()){
+            add(key, spikes.get(key));
+        }
+
+        for (String key: icons.keySet()){
+            add(key, icons.get(key));
+        }
+
+        for (String key: metalls.keySet()){
+            add(key, metalls.get(key));
+        }
+
+        for (String key: powers.keySet()){
+            add(key, powers.get(key));
+        }
 
         for(String key: base.keySet()) {
             add(key, base.get(key));
