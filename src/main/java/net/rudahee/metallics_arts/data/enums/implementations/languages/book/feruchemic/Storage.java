aeeb3d839e73def77d7e30e133f3ee965e9a28fd
kantarowskimichal <kantarowskimichal@gmail.com>
2023-02-16 12:59:08 +0000

    added polish translations to enums, fixed some minor mistakes in english translations

--- src/main/java/net/rudahee/metallics_arts/data/enums/implementations/languages/book/feruchemic/Storage.java
+++ src/main/java/net/rudahee/metallics_arts/data/enums/implementations/languages/book/feruchemic/Storage.java
@@ -2,55 +2,81 @@ package net.rudahee.metallics_arts.data.enums.implementations.languages.book.fer
 
 public enum Storage {
     IRON("++Storing:++\\\n\\\nx As you store, you seem to become lighter.",
-            "++Almacenar:++\\\nx Mientras almacenas, notas como el mundo tira de ti con menos fuerza."),
+            "++Almacenar:++\\\nx Mientras almacenas, notas como el mundo tira de ti con menos fuerza.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się lżejszy."),
     STEEL("++Storing:++\\\n\\\nx While you store you feel really sluggish.",
-            "++Almacenar:++\\\nx Mientras almacenas, notas como tu cuerpo responde más lentamente de lo que debería. ¿Ese caracol te acaba de avanzar?"),
+            "++Almacenar:++\\\nx Mientras almacenas, notas como tu cuerpo responde más lentamente de lo que debería. ¿Ese caracol te acaba de avanzar?",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się bardzo powolny."),
     TIN("++Storing:++\\\n\\\nx While you store, heighten your senses.",
-            "++Almacenar:++\\\nx Mientras almacenas, disminuye tus sentidos."),
+            "++Almacenar:++\\\nx Mientras almacenas, disminuye tus sentidos.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia wyostrz swoje zmysły."),
     PEWTER("++Storing:++\\\n\\\nx While you decant, you feel weaker, I could almost win you a chicken.",
-            "++Almacenar:++\\\nx Mientras almacenas, te sientes más debil, casi te podria ganar una gallina."),
+            "++Almacenar:++\\\nx Mientras almacenas, te sientes más debil, casi te podria ganar una gallina.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się słaby."),
     ZINC("++Storing:++\\\n\\\nx While storing, deny all loot from entity.",
-            "++Almacenar:++\\\nx Mientras almacenas, te cuesta muchísimo encontrar objetos de los seres a los que aniquilas."),
+            "++Almacenar:++\\\nx Mientras almacenas, te cuesta muchísimo encontrar objetos de los seres a los que aniquilas.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia przedmioty nie wypadają z istot."),
     BRASS("++Storing:++\\\n\\\nx While you store, it allows you to store your heat, making you less vulnerable to heat sources.\\\n\\\nx Be careful to use it in cold places.",
-            "++Almacenar:++\\\nx Mientras almacenas, te permite guardar tu calor corporal, haciéndote más resistente a las fuentes de calor.\\\n\\\nx Poco recomendable almacenar en lugares fríos por... obvios motivos."),
+            "++Almacenar:++\\\nx Mientras almacenas, te permite guardar tu calor corporal, haciéndote más resistente a las fuentes de calor.\\\n\\\nx Poco recomendable almacenar en lugares fríos por... obvios motivos.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia, odkładasz swoje ciepło do metal myśli, ochładzająć cię.\\\n\\\nx Uważaj na zimne miejsca"),
     COPPER("++Storing:++\\\n\\\nx While you store, you lose your experience.",
-            "++Almacenar:++\\\nx Mientras almacenas, guardas tu experiencia en la mente de metal."),
+            "++Almacenar:++\\\nx Mientras almacenas, guardas tu experiencia en la mente de metal.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia odkładasz doświadczenie do metal myśli."),
     BRONZE("++Storing:++\\\n\\\nx While storing, you will feel tired at all times, and so Phantoms will appear near you.",
-            "++Almacenar:++\\\nx Mientras almacenas, vas a sentirte cansado en todo momento, por lo que aparecerán Fantasmas a tu altrededor (incluso de día)."),
+            "++Almacenar:++\\\nx Mientras almacenas, vas a sentirte cansado en todo momento, por lo que aparecerán Fantasmas a tu altrededor (incluso de día).",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się zmęczony, przez co fantomy będą pojawiały się w twoim pobliżu."),
     ALUMINUM("++Storing:++\\\n\\\nx By storing, you remove the Identity of a metalmind.",
-            "++Almacenar:++\\\nx Al almacenar, quitas la Identidad de una mente de metal."),
+            "++Almacenar:++\\\nx Al almacenar, quitas la Identidad de una mente de metal.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia usuwasz tożsamość z metal myśli."),
     DURALUMIN("++Storing:++\\\n\\\nx While you store, you seem disconnected from the world, it almost seems like it is rejecting you.",
-            "++Almacenar:++\\\nx Mientras almacenas, pareces desconectado del mundo, como si este te estuviera rechazando. Y sí, esa frase tiene sentido."),
+            "++Almacenar:++\\\nx Mientras almacenas, pareces desconectado del mundo, como si este te estuviera rechazando. Y sí, esa frase tiene sentido.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się oderwany od świata, prawie tak jak by sama żeczywistość cię odrzucała."),
     CHROMIUM("++Storing:++\\\n\\\nx While you store, it seems that luck is not on your side, it almost seems that you are going to get sick.",
-            "++Almacenar:++\\\nx Mientras almacenas, parece que Armonía te ha dado la espalda, porque no paran de sucederte desgracias injustificables."),
+            "++Almacenar:++\\\nx Mientras almacenas, parece que Armonía te ha dado la espalda, porque no paran de sucederte desgracias injustificables.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się szczęście jest po twojej stronie."),
     NICROSIL("++Storing:++\\\n\\\nx To store, it seems like your other metal minds are never going to fill up. It is necessary to use it together with other powers.",
-            "++Almacenar:++\\\nx Para almacenar, requieres el sacrificio de los atributos que haya en otras mentes de metal que lleves encima. Es necesario usarla junto a otros poderes."),
+            "++Almacenar:++\\\nx Para almacenar, requieres el sacrificio de los atributos que haya en otras mentes de metal que lleves encima. Es necesario usarla junto a otros poderes.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia wydaje się jak by twoije inne metal myśli w ogóle się nie napełniały. Koniecznym jest używanie tej mocy wraz z innymi."),
     GOLD("++Storing:++\\\n\\\nx While you store, you seem to feel unwell, it almost seems that you are going to die.",
-            "++Almacenar:++\\\nx Mientras almacenas, parece que te vuelves un contenedor de malestar y náuseas. Vigila tus huesos, quizá se rompen más rápido de lo que deberían."),
+            "++Almacenar:++\\\nx Mientras almacenas, parece que te vuelves un contenedor de malestar y náuseas. Vigila tus huesos, quizá se rompen más rápido de lo que deberían.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia czujesz się chory, prawie tak jak byś miał umrzeć."),
     ELECTRUM("++Storing:++\\\n\\\nx While you store, it seems that your life is drastically reduced.",
-            "++Almacenar:++\\\nx Mientras almacenas, sientes que tu esperanza de vida se reduce drásticamente."),
+            "++Almacenar:++\\\nx Mientras almacenas, sientes que tu esperanza de vida se reduce drásticamente.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia twoje zdrowie drastycznie się zmniejsza."),
     CADMIUM("++Storing:++\\\n\\\nx While storing, you will have difficulty breathing even out of the water.",
-            "++Almacenar:++\\\nx Mientras almacenas, tendrás dificultades para respirar... incluso fuera del agua."),
+            "++Almacenar:++\\\nx Mientras almacenas, tendrás dificultades para respirar... incluso fuera del agua.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia będziesz miał trudności z oddychaniem, nawet poza wodą."),
     BENDALLOY("++Storing:++\\\n\\\nx While you store, you constantly increase your hunger",
-            "++Almacenar:++\\\nx Mientras almacenas, aumentas tu hambre constantemente."),
+            "++Almacenar:++\\\nx Mientras almacenas, aumentas tu hambre constantemente.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia ciągle czujesz się głodny."),
     ATIUM("++Storing:++\\\n\\\nx As you decant, it allows you to shine for other players.",
-            "++Almacenar:++\\\nx Mientras almacenas, brillas para el resto de jugadores (luminiscencia)."),
+            "++Almacenar:++\\\nx Mientras almacenas, brillas para el resto de jugadores (luminiscencia).",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia wszyscy gracze są w stanie wyczuć twoją obecność."),
     MALATIUM("++Storing:++\\\n\\\nx While you store, you store the item's durability in your hand, so you can use it later.",
-            "++Almacenar:++\\\nx Mientras almacenas, guardas la durabilidad del objeto que estés sujetando con la mano en ese instante, incluso pudiendo llegar a romperlo."),
+            "++Almacenar:++\\\nx Mientras almacenas, guardas la durabilidad del objeto que estés sujetando con la mano en ese instante, incluso pudiendo llegar a romperlo.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia odkładasz wytrzymałość przedmiotu w ręce do metalmyśli."),
     LERASIUM("++Storing:++\\\n\\\nx When you store, you will save your current Allomantic reserves.",
-            "++Almacenar:++\\\nx Cuando almacenas, guardaras las reservas alománticas que hubieras ingerido."),
+            "++Almacenar:++\\\nx Cuando almacenas, guardaras las reservas alománticas que hubieras ingerido.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia odkładasz swoje aktualne alomantyczne rezerwy."),
     ETTMETAL("++Storing:++\\\n\\\nx When you store, it seems that the energy from the explosions that hit you goes somewhere else.",
-            "++Almacenar:++\\\nx Cuando almacenas, la energía de las explosiones no solo rompe tu cuerpo, sino que se guarda en la mente de metal.");
+            "++Almacenar:++\\\nx Cuando almacenas, la energía de las explosiones no solo rompe tu cuerpo, sino que se guarda en la mente de metal.",
+            "++Gromadzenie:++\\\n\\\nx Podczas gromadzenia wydaje się że energia z wybuchuw zostaje przekierowana gdzie indziej.");
     private final String english;
     private final String spanish;
+    private final String polish;
 
-    Storage(String english, String spanish) {
+    Storage(String english, String spanish, String polish) {
         this.english = english;
         this.spanish = spanish;
+        this.polish = polish;
     }
 
     public String getEnglish() {
         return english;
+    }
+
+    public String getPolish() {
+        return polish;
     }
 
 }
--- src/main/java/net/rudahee/metallics_arts/data/enums/implementations/languages/book/feruchemic/Tap.java
+++ src/main/java/net/rudahee/metallics_arts/data/enums/implementations/languages/book/feruchemic/Tap.java
@@ -2,52 +2,74 @@ package net.rudahee.metallics_arts.data.enums.implementations.languages.book.fer
 
 public enum Tap {
     IRON("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, you seem to get much heavier.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes como te vuelves mucho más pesado. Esperemos que no lo hagas en los dos sentidos de la expresión."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes como te vuelves mucho más pesado. Esperemos que no lo hagas en los dos sentidos de la expresión.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, stajesz się dużo cięższy."),
     STEEL("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, you feel really fast.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que puedes dejar atrás el viento."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que puedes dejar atrás el viento.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, stajesz się bardzo szybki."),
     TIN("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, diminish your senses.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, aumenta tus sentidos."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, aumenta tus sentidos.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, stłum swoje zmysły."),
     PEWTER("\\\n\\\n++Tapping:++\\\n\\\n x While you store, you feel much stronger, you could kill the Wither boss with your fists.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sientes mucho más fuerte, como si pudieras matar al Wither a puñetazos."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sientes mucho más fuerte, como si pudieras matar al Wither a puñetazos.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, stajesz się dużo silniejszy."),
     ZINC("\\\n\\\n++Tapping:++\\\n\\\n x While you decant, you have a chance to double the loot.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que todos los seres están a rebosar de objetos que puedes (y quieres) obtener."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que todos los seres están a rebosar de objetos que puedes (y quieres) obtener.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, masz szansę na podwojenie przedmiotów wypadających z istot."),
     BRASS("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, you expel heat, causing you to set other entities on fire on hit.\\\n\\\n x Comforting against that cold powder snow.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, expulsas el calor corporal acumulado, haciendo que puedas prender en llamas a otras entidades al golpearlas.$(br2)\\\nx Útil para combatir el frío, además."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, expulsas el calor corporal acumulado, haciendo que puedas prender en llamas a otras entidades al golpearlas.$(br2)\\\nx Útil para combatir el frío, además.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania wydzielasz ciepło, podpalając istoty które uderzysz\\\n\\\nx Przydatne w chłodnych miejscach."),
     COPPER("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, you gain back all of your experience.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, ganas toda la experiencia que hubiera en la mente de metal."),
-    BRONZE("\\\n\\\n++Tapping:++\\\n\\\n x As you tapping, you will feel more rested, causing the Phantoms to ignore you.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sentirás mas descansado, haciendo que los Fantasmas que pueda haber te ignoren."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, ganas toda la experiencia que hubiera en la mente de metal.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania odzyskujesz doświadczenie z metal myśli."),
+    BRONZE("\\\n\\\n++Tapping:++\\\n\\\n x While tapping, you will feel more rested, causing the Phantoms to ignore you.",
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sentirás mas descansado, haciendo que los Fantasmas que pueda haber te ignoren.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania czujesz się wypoczęty, fantomy będą cię ignorowały."),
     ALUMINUM("\\\n\\\n++Tapping:++\\\n\\\n x By decanting, you seal your Identity in a metalmind.",
-            "\\\n\\\n++Decantar:++\\\nx Al decantar, sellas una mente de metal con tu Identidad."),
+            "\\\n\\\n++Decantar:++\\\nx Al decantar, sellas una mente de metal con tu Identidad.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania pieczętujesz tożsamość w metal myśli."),
     DURALUMIN("\\\n\\\n++Tapping:++\\\n\\\n x While you decant, it seems that you find the paths with your eyes closed, it seems as if it were your land.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, parece que encuentras los caminos con los ojos cerrados, justo lo contrario que te sucedía antes. Lo entenderás cuando lo pruebes."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, parece que encuentras los caminos con los ojos cerrados, justo lo contrario que te sucedía antes. Lo entenderás cuando lo pruebes.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania masz odczucie jak byś żył na tych ziemiach od zawsze, wydaje się że jesteś w stanie poruszać się po nich z zamkniętymi oczami."),
     CHROMIUM("\\\n\\\n++Tapping:++\\\n\\\n x While you decant it, you have extraordinary luck, is it time to go exploring?",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, casi es como si Dios te hubiera bendecido. ¿Sería ese un momento para salir a explorar?"),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, casi es como si Dios te hubiera bendecido. ¿Sería ese un momento para salir a explorar?",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania szczęście jest po twojej stronie."),
     NICROSIL("\\\n\\\n++Tapping:++\\\n\\\n x When decanting, it seems like your other metal minds are never going to run out. It is necessary to use it together with other powers.",
-            "\\\n\\\n++Decantar:++\\\nx Al decantar, logras estirar la duración de otras mentes de metal que estés decantando. Es necesario usarla junto a otros poderes."),
+            "\\\n\\\n++Decantar:++\\\nx Al decantar, logras estirar la duración de otras mentes de metal que estés decantando. Es necesario usarla junto a otros poderes.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania wydaje się że twoje pozostałe metal myśli nigdy się nie skończą, ta moc musi być używana wraz z innymi."),
     GOLD("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, it looks like you could take on any disease.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que no hay bicho en el mundo que pueda dañarte. Tu cuerpo está hecho para absorber el dolor."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, sientes que no hay bicho en el mundo que pueda dañarte. Tu cuerpo está hecho para absorber el dolor.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania wydaje się że był byś w stanie stawić czoła każdej ranie i chorobie."),
     ELECTRUM("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, it seems that your life is increased drastically.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, es como si de repente te vieras capaz de vivir más años que cualquier otro ser humano."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, es como si de repente te vieras capaz de vivir más años que cualquier otro ser humano.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania twoje życie zostaje drastycznie zwiększone."),
     CADMIUM("\\\n\\\n++Tapping:++\\\n\\\n x While you decant, you will feel like a fish in water, it almost seems that you can breathe at the bottom of the ocean.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sentirás como pez en el agua, hasta el punto que casi creerás que no necesitas salir a buscar aire cuando bucees."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te sentirás como pez en el agua, hasta el punto que casi creerás que no necesitas salir a buscar aire cuando bucees.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania nigdy nie zabraknie ci oddechu."),
     BENDALLOY("\\\n\\\n++Tapping:++\\\n\\\n x As you decant, you constantly reduce your hunger",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, reduces tu hambre constantemente."),
-    ATIUM("\\\n\\\n++Tapping:++\\\n\\\n x While you store, it allows you to become invisible.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te vuelves invisible."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, reduces tu hambre constantemente.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania nigdy nie czujesz się głodny."),
+    ATIUM("\\\n\\\n++Tapping:++\\\n\\\n x While tapping, you become invisible.",
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, te vuelves invisible.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania, stajesz się niewidzialny."),
     MALATIUM("\\\n\\\n++Tapping:++\\\n\\\n x While you decant, the durability of the item in hand increases, thanks to your reserves.",
-            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, aumenta la durabilidad del objeto que tengas en la mano. Aunque la durabilidad no es universal, aviso."),
+            "\\\n\\\n++Decantar:++\\\nx Mientras decantas, aumenta la durabilidad del objeto que tengas en la mano. Aunque la durabilidad no es universal, aviso.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania wytrzymałość przedmiotu w twojej ręce wzrasta."),
     LERASIUM("\\\n\\\n++Tapping:++\\\n\\\n x When you decant, you gain the Allomantic reserves you previously stored.",
-            "\\\n\\\n++Decantar:++\\\nx Cuando decantas, obtienes las reservas alománticas que hubiera en la mente de metal."),
+            "\\\n\\\n++Decantar:++\\\nx Cuando decantas, obtienes las reservas alománticas que hubiera en la mente de metal.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania odzyskujesz odłożone rezerwy allomantyczne."),
     ETTMETAL("\\\n\\\n++Tapping:++\\\n\\\n x When decanting, it seems that you are going to release all your inner energy at once.",
-            "\\\n\\\n++Decantar:++\\\nx Cuando decantas, sientes como si tu interior fuera a, literalmente, explotar.");
+            "\\\n\\\n++Decantar:++\\\nx Cuando decantas, sientes como si tu interior fuera a, literalmente, explotar.",
+            "\\\n\\\n++Czerpanie:++\\\n\\\n x Podczas czerpania uwalniasz całą zgromadzoną energię w jednej chwili.");
 
     private final String english;
     private final String spanish;
+    private final String polish;
 
-    Tap(String english, String spanish) {
+    Tap(String english, String spanish, String polish) {
         this.english = english;
         this.spanish = spanish;
+        this.polish = polish;
     }
 
     public String getEnglish() {
@@ -56,5 +78,8 @@ public enum Tap {
 
     public String getSpanish() {
         return spanish;
+    }
+    public String getPolish() {
+        return polish;
     }
 }
