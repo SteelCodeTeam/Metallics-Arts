88fcc90591210fe31f48fdd546466a88c0581eb1
kantarowskimichal <kantarowskimichal@gmail.com>
2023-04-01 16:25:12 +0100

    added 3d metal lines

--- src/main/java/net/rudahee/metallics_arts/modules/logic/client/client_events/OnRenderLevelStage.java
+++ src/main/java/net/rudahee/metallics_arts/modules/logic/client/client_events/OnRenderLevelStage.java
@@ -1,9 +1,12 @@
 package net.rudahee.metallics_arts.modules.logic.client.client_events;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -11,6 +14,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.rudahee.metallics_arts.MetallicsArts;
 import net.rudahee.metallics_arts.data.enums.implementations.LinesColorEnum;
 import net.rudahee.metallics_arts.data.enums.implementations.MetalTagEnum;
 import net.rudahee.metallics_arts.data.player.IInvestedPlayerData;
@@ -21,6 +25,8 @@ import net.rudahee.metallics_arts.utils.FoundNearbyMetalUtils;
 import net.rudahee.metallics_arts.utils.powers_utils.MetalBlockUtils;
 
 import javax.annotation.Nullable;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -31,9 +37,8 @@ public class OnRenderLevelStage {
 
     @OnlyIn(Dist.CLIENT)
     public static void onRenderLevelStage(RenderLevelStageEvent event, Minecraft minecraft, @Nullable Player player, @Nullable IInvestedPlayerData capability) throws PlayerException {
-        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_PARTICLES
-                                    || event.getStage() != RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS
-                                    || minecraft.options.getCameraType().isMirrored()) {
+
+        if ((event.getStage() != RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS) || minecraft.options.getCameraType().isMirrored()) {
             return;
         }
 
@@ -52,8 +57,22 @@ public class OnRenderLevelStage {
             return;
         }
 
+        Matrix4f transformationMatrix = DrawUtils.setUpForDrawingQuadLines(event);
+
+        if (capability.isBurning(MetalTagEnum.IRON) || capability.isBurning(MetalTagEnum.STEEL)){
+            Vec3 view = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+            Vector3f lookDirection = Minecraft.getInstance().gameRenderer.getMainCamera().getLookVector();
+            Vec3 lookDir = new Vec3(lookDirection.x(), lookDirection.y(), lookDirection.z());
+            Vec3 playerPos = Minecraft.getInstance().player.getPosition(event.getPartialTick()).add(new Vec3(0,1,0)).add(lookDir);
+            ResourceLocation texture = new ResourceLocation(MetallicsArts.MOD_ID,
+                    "textures/veffects/the_idle4_60_transluscent.png");
+            Vec3 sourceCameraVector = view.subtract(playerPos);
+            metalLines(event.getRenderTick(), event.getPartialTick(), view, transformationMatrix, playerPos, 0.005, texture, 31, 8, 32,
+                    FoundNearbyMetalUtils.getMetalEntities().stream().sorted(Comparator.comparingDouble(e -> -1*(playerPos.subtract(e.getPosition(event.getPartialTick())).normalize().dot(sourceCameraVector.normalize())))).toList(),
+                    FoundNearbyMetalUtils.getMetalBlocks().stream().sorted(Comparator.comparingDouble(e -> -1*(playerPos.subtract(e.getCenter()).normalize().dot(sourceCameraVector.normalize())))).toList());
+        }
+
         PoseStack stack = DrawUtils.setupPoseStack(event, minecraft);
-        stack.pushPose();
 
         double rho = 1;
         float theta = (float) ((player.getViewYRot(event.getPartialTick()) + 90) * Math.PI / 180);
@@ -67,11 +86,11 @@ public class OnRenderLevelStage {
             DrawUtils.patchPoseForATIGraphicCards();
         }
 
-        if (capability.isBurning(MetalTagEnum.IRON)) {
-            IronMetalLines(stack, playerCameraVector, FoundNearbyMetalUtils.getMetalEntities(), FoundNearbyMetalUtils.getMetalBlocks());
-        } else if (capability.isBurning(MetalTagEnum.STEEL)) {
-            SteelMetalLines(stack, playerCameraVector, FoundNearbyMetalUtils.getMetalEntities(), FoundNearbyMetalUtils.getMetalBlocks());
-        }
+//        if (capability.isBurning(MetalTagEnum.IRON)) {
+//            IronMetalLines(stack, playerCameraVector, FoundNearbyMetalUtils.getMetalEntities(), FoundNearbyMetalUtils.getMetalBlocks());
+//        } else if (capability.isBurning(MetalTagEnum.STEEL)) {
+//            SteelMetalLines(stack, playerCameraVector, FoundNearbyMetalUtils.getMetalEntities(), FoundNearbyMetalUtils.getMetalBlocks());
+//        }
 
         if (capability.isBurning(MetalTagEnum.BRONZE)) {
             // /!\ REDO LOGIC /!\
@@ -91,6 +110,15 @@ public class OnRenderLevelStage {
         }
 
         DrawUtils.teardownPoseStack(stack);
+    }
+
+    private static void metalLines(int tick, float partialTick, Vec3 viewPosition, Matrix4f translationMatrix, Vec3 source, double scale, ResourceLocation texture, int numberOfFrames, int columnWidth, int columnHeight, List<Entity> entities, List<MetalBlockUtils> blocks){
+        for (Entity entity: entities) {
+            DrawUtils.drawMetalQuadLines(tick, viewPosition, translationMatrix, entity.getPosition(partialTick), source, scale, texture, numberOfFrames, columnWidth, columnHeight);
+        }
+        for (MetalBlockUtils block : blocks) {
+            DrawUtils.drawMetalQuadLines(tick, viewPosition, translationMatrix, block.getCenter(), source, scale + (scale * 0.1) * block.size(), texture, numberOfFrames, columnWidth, columnHeight);
+        }
     }
 
     public static void setRespawnPos(GlobalPos pos) {
