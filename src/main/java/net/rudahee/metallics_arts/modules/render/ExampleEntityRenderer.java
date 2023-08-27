9e34879b7b81bb0467aed7b784fc1b8dbd884ff1
PauGallego <paugf2004@gmail.com>
2023-08-27 17:44:10 +0200

    Added Example entity for future implementations

---
+++ src/main/java/net/rudahee/metallics_arts/modules/render/ExampleEntityRenderer.java
@@ -0,0 +1,21 @@
+package net.rudahee.metallics_arts.modules.render;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.rudahee.metallics_arts.MetallicsArts;
+import net.rudahee.metallics_arts.modules.custom_entities.ExampleEntity;
+import net.rudahee.metallics_arts.modules.custom_entities.ExampleEntityModel;
+
+public class ExampleEntityRenderer extends MobRenderer<ExampleEntity, ExampleEntityModel> {
+
+    private static final ResourceLocation TEXTURE = new ResourceLocation (MetallicsArts.MOD_ID,"textures/example_entity.png");
+    public ExampleEntityRenderer(EntityRendererProvider.Context ctx) {
+        super(ctx, new ExampleEntityModel(ctx.bakeLayer(ExampleEntityModel.LAYER_LOCATION)), 0.5f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(ExampleEntity entity) {
+        return TEXTURE;
+    }
+}
--- src/main/java/net/rudahee/metallics_arts/setup/registries/ModRenderRegister.java
+++ src/main/java/net/rudahee/metallics_arts/setup/registries/ModRenderRegister.java
@@ -6,7 +6,9 @@ import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.rudahee.metallics_arts.MetallicsArts;
+import net.rudahee.metallics_arts.modules.custom_entities.ExampleEntityModel;
 import net.rudahee.metallics_arts.modules.logic.client.custom_renders.ModEntityRender;
+import net.rudahee.metallics_arts.modules.render.ExampleEntityRenderer;
 
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = MetallicsArts.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -15,5 +17,12 @@ public class ModRenderRegister {
     @SubscribeEvent
     public static void register(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ModEntityTypesRegister.BULLET_PROJECTILE.get(), ModEntityRender::new);
+        event.registerEntityRenderer(ModEntityTypesRegister.EXAMPLE.get(), ExampleEntityRenderer::new);
     }
+
+    @SubscribeEvent
+    public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event){
+        event.registerLayerDefinition(ExampleEntityModel.LAYER_LOCATION, ExampleEntityModel::createBodyLayer);
+    }
+
 }
